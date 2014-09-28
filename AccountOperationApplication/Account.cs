bc9a872a33ee9bece81834add11e9ef8d730635f
floatingFaez <floatingfaez@live.com>
2014-09-28 16:32:51 +0600

    Initial Project File Uploaded

---
+++ AccountOperationApplication/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationApplication
+{
+    class Account
+    {
+        private string name;
+        private string number;
+        private double balance;
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public string Number
+        {
+            set
+            {
+                number = value;
+            }
+            get
+            {
+                return number;
+            }
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+        }
+
+        public void DoDeposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void DoWitdraw(double amount)
+        {
+            balance -= amount;
+        }
+
+    }
+}
---
+++ AccountOperationApplication/AccountOperation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationApplication
+{
+    public partial class AccountOperationForm : Form
+    {
+        public AccountOperationForm()
+        {
+            InitializeComponent();
+        }
+
+        private Account anAccount;
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            if (customerNameTextBox.Text != string.Empty && accountNumberTextBox.Text != string.Empty)
+            {
+                anAccount = new Account();
+                anAccount.Name = customerNameTextBox.Text;
+                anAccount.Number = accountNumberTextBox.Text;
+
+                customerNameTextBox.Text = string.Empty;
+                accountNumberTextBox.Text = string.Empty;
+                MessageBox.Show(@"Your Account has been created !!");
+            }
+            else
+            {
+                MessageBox.Show(@"Please enter acount number and customer name !!");
+            }
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            if (anAccount != null)
+            {
+                anAccount.DoDeposit(Convert.ToDouble(amountTextBox.Text));    
+            }
+            else
+                MessageBox.Show(@"Create an account first");    
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            if (anAccount != null)
+            {
+            anAccount.DoWitdraw(Convert.ToDouble(amountTextBox.Text));
+            }
+            else
+                 MessageBox.Show(@"Create an account first");
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            if (anAccount != null)
+            {
+            string msg = anAccount.Name + ", your account number is " + anAccount.Number + " and it's balance : " +
+                         anAccount.Balance;
+            MessageBox.Show(msg);
+            }
+            else
+                MessageBox.Show(@"Create an account first");
+        }
+    }
+}
